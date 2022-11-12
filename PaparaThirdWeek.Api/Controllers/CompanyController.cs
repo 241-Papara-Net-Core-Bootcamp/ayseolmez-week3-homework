2bb64cce71b40dcb39c5b5bb6fda5a92655d6d74
ayseolmez <ayseozdemir3408@gmail.com>
2022-11-12 21:37:27 +0300

    Add project files.

---
+++ PaparaThirdWeek.Api/Controllers/CompanyController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PaparaThirdWeek.Domain.Entities;
+using PaparaThirdWeek.Services.Abstracts;
+using PaparaThirdWeek.Services.DTOs;
+using System.Threading.Tasks;
+
+namespace PaparaThirdWeek.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly ICompanyService companyService;
+
+        public CompanyController(ICompanyService companyService)
+        {
+            this.companyService = companyService;
+        }
+
+        [HttpPost]
+        public IActionResult Post(CompanyDto company)
+        {
+            var companyDto = new Company
+            {
+                Name = company.Name,
+                Adress = company.Adress,
+                City = company.City,
+                CreatedBy = "Samet",
+                CreatedDate = System.DateTime.Now,
+                Email = company.Email,
+                IsDeleted = false,
+                TaxNumber = company.TaxNumber
+            };
+            companyService.Add(companyDto);
+            return Ok();
+        }
+
+        [HttpGet("AllCompanies")]
+        public IActionResult Get()
+        {
+            var result = companyService.GetAll();
+            return Ok(result);   
+        }
+
+        [HttpGet("GetById")]
+        public IActionResult GetById(int id)
+        {
+            companyService.GetById(id);
+            return Ok();
+        }
+
+        [HttpPut("Update")]
+        public IActionResult Update(Company company)
+        {
+            companyService.Update(company);
+                return Ok();
+        }
+
+        [HttpDelete("Delete")]
+        public IActionResult HardRemove(Company company)
+        {
+            companyService.HardRemove(company);
+            return Ok();
+        }
+
+        
+
+
+
+    }
+}
---
+++ PaparaThirdWeekServices/Abstracts/ICompanyService.cs
@@ -0,0 +1,22 @@
+﻿using PaparaThirdWeek.Domain.Entities;
+using PaparaThirdWeek.Services.DTOs;
+using System.Collections.Generic;
+
+namespace PaparaThirdWeek.Services.Abstracts
+{
+    public interface ICompanyService
+    {
+        public IEnumerable<Company> GetAll();
+        public void Add(Company company);
+
+        public void Update(Company company);
+
+        public void Remove(Company company);
+
+        public void HardRemove(Company company);
+
+        public void GetById(int id);
+
+
+    }
+}
